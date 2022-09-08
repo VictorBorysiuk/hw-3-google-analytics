176fe5826f03184dffdb74c2a862efe1a4f48ed4
Viktor Borysiuk <borysyukviktor@gmail.com>
2022-09-08 19:33:32 +0300

    Added env param.

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,11 @@ const app = express();
 app.enable('trust proxy');
 
 mongoose
-.connect(
-  "mongodb+srv://Viktor:" +
+  .connect(
+    "mongodb+srv://Viktor:" +
     process.env.MONGO_ATLAS_PW +
     "@cluster0.yivkoun.mongodb.net/test?retryWrites=true&w=majority"
-)
+  )
   .then(() => {
     console.log("Connected to database!");
   })
@@ -27,21 +27,20 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
-// app.use("/images", express.static(__dirname + "/images"));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
- app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-   );
-   res.setHeader(
-     "Access-Control-Allow-Methods",
-     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-   );
-   next();
- });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
@@ -59,33 +58,50 @@ async function getDataAboutNBUExchange() {
   if (dd < 10) dd = '0' + dd;
   if (mm < 10) mm = '0' + mm;
   const formattedToday = dd + '.' + mm + '.' + yyyy;
-  const bodyNBU = { "r030":840,"txt":"Долар США","cc":"USD","exchangedate": `${ formattedToday }`};
-  await fetch(urlNBU,{method: 'GET', body: JSON.stringify(bodyNBU)}).then((response) => {
+  const bodyNBU = { "r030": 840, "txt": "Долар США", "cc": "USD", "exchangedate": `${formattedToday}` };
+  await fetch(urlNBU, { method: 'GET', body: JSON.stringify(bodyNBU) }).then((response) => {
     return response.json();
   })
-  .then((data) => {
-    dataAboutNBUExchange = data;
-  });
+    .then((data) => {
+      dataAboutNBUExchange = data;
+    }).catch(function (err) {
+      console.log("Promise https://bank.gov.ua Rejected" + err);
+    });
 
   return dataAboutNBUExchange || {};
 }
 
-async function gaCollect(dataForGoogleAnalytics){
-  const rate = value[0].rate || 0;
-  const body = { "ea":"exchange_rate", "ec":"current_uah_usd_ratio","el":"exchange_rate_label","ev":`${ rate }`}
-  const url = `http://www.google-analytics.com/collect?v=2&tid=${ process.env.MONGO_ATLAS_PW }`
-  const res = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+async function gaCollect(dataForGoogleAnalytics) {
+  const rate = dataForGoogleAnalytics[0].rate;
+
+  if (rate) {
+    await fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${ process.env.MEASUREMENT_ID }&api_secret=${ process.env.API_SECRET }`, {
+      method: "POST",
+      body: JSON.stringify({
+        "client_id": "595076620.1662328068",
+        "events": [{
+          "name": "exchange_rate",
+          "params": {
+            "currency": "USD",
+            "value": `${rate}`,
+          }
+        }]
+      })
+    }).then(function () {
+      console.log("Sended to google-analytics exchange_rate (uah/usd) " + rate);
+    }).catch(function (err) {
+      console.log("Promise google-analytics Rejected" + err);
+    });
+  }
 }
-const periodInOneDay = 24 * 60 * 60 * 1000;
-setInterval (function () {
-  const dataForGoogleAnalytics = await getDataAboutNBUExchange();
-  gaCollect(dataForGoogleAnalytics);
-}, period)
+const periodInOneDay = 10000;
+setInterval(async function () {
+  const dataForGoogleAnalytics = await getDataAboutNBUExchange()
+  gaCollect(dataForGoogleAnalytics).then(function () {
+    // console.log("Promise gaCollect Resolved");
+  }).catch(function (err) {
+    console.log("Promise gaCollect Rejected" + err);
+  });;
+}, periodInOneDay);
 
 module.exports = app;
